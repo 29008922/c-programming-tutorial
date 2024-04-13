f1e7387eb289e9a37fc82a69fa209a2157c6aa4d
haron murumba <haron.murumba22@gmail.com>
2024-04-13 17:25:05 +0100

    teperatur-ferrnheight convertor

---
+++ TKINTER/windows.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#import tkinter as tk
+
+#window = tk.Tk()
+#greeting = tk.Label(text='Hello, world')
+#greeting.pack()
+#window.mainloop()
+
+# different approach 
+
+import tkinter as tk
+console = tk.Tk()
+label = tk.Label(
+    text='my python wondow',
+    fb='green',
+    bg='black'
+    )
+label.pack()
+console.mainloop()
+
